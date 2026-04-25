7547e5f2e13f0e68bc479d36133acc928f895012
wcsouzaponce <wcsouzaponce@gmail.com>
2026-04-25 10:12:56 -0700

    Add files via upload

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    //float a = Mathf.Sin(5);
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.position = player.transform.position + offset;
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
